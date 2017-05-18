0a9cc324ad31e11a7ce0ac1f219954e573a8c8cb
Esteban Barajas <estebangit@users.noreply.github.com>
2017-05-18 09:37:38 +0200

    Change logger to use slf4j.

--- JEETemplate-business/src/main/java/com/esteban/template/ejb/session/HelloEJB.java
+++ JEETemplate-business/src/main/java/com/esteban/template/ejb/session/HelloEJB.java
@@ -1,19 +1,19 @@
 package com.esteban.template.ejb.session;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.ejb.AsyncResult;
 import javax.ejb.Asynchronous;
 import javax.ejb.Stateless;
-import java.util.Date;
 import java.util.concurrent.Future;
-import java.util.logging.Logger;
-
 /**
  * Created by esteban on 20.03.17.
  */
 @Stateless
 public class HelloEJB implements IHelloLocal, IHelloRemote {
 
-    private static final Logger log = Logger.getLogger(HelloEJB.class.getName());
+    private static final Logger log = LoggerFactory.getLogger(HelloEJB.class);
 
     private final long sleepTime = 5000;
 
@@ -29,6 +29,7 @@ public class HelloEJB implements IHelloLocal, IHelloRemote {
         try {
             Thread.sleep(sleepTime);
         } catch (InterruptedException e) {
+            log.trace(e.getMessage(), e);
         }
         log.info("returning the result");
         return new AsyncResult<>("Hello " + name);
